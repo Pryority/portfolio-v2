853e20fdf4e6814a3364d2f524d40dd41d4149f8
pryority <matthewapryor@gmail.com>
2022-09-27 14:34:46 -0400

    fix: hardcoded sanity projectId into sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: process.env.SANITY_PROJECT_ID!,
+    projectId: process.env.SANITY_PROJECT_ID! || 'l92xza2r',
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production'
 }
