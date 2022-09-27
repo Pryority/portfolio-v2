fb62a4551c50ab16ae49ed167b6d511ea9780daf
pryority <matthewapryor@gmail.com>
2022-09-27 13:50:23 -0400

    fix: change

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,8 +1,8 @@
 import { PageInfo } from '../typings'
 
-export const fetchPageInfo =  () => {
+export const fetchPageInfo = async () => {
     const res =  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
-    const data = JSON.parse(JSON.stringify(res))
+    const data = await (await res).json()
 
     const pageInfo: PageInfo = data.pageInfo
 
