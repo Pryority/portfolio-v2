5e854e6acddc54e18cc3ccef39196047609b8404
pryority <matthewapryor@gmail.com>
2022-09-27 16:38:22 -0400

    fix: typo in fetchExperience.ts

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,6 +1,6 @@
 import { Experience } from '../typings'
 
-export async function fetchExperiences() {
+export async function fetchExperience() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
 
     const data = await res.json()
