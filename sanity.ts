34f0caa5b6b4cf2942f6f3998c7aaea22cfc6c53
pryority <matthewapryor@gmail.com>
2022-09-28 11:38:02 -0400

    change projectId value

--- sanity.ts
+++ sanity.ts
@@ -3,11 +3,11 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: 'l92xza2r',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production'
 }
 
 export const sanityClient = createClient(config);
 
-export const urlFor = (source: any) => createImageUrlBuilder(config).image(source) 
+export const urlFor = (source: any) => createImageUrlBuilder(config).image(source)
