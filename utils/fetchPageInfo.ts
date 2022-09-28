0c67e155f80e9127662480f2f46e4b52e138ff45
pryority <matthewapryor@gmail.com>
2022-09-28 14:33:28 -0400

    untracked files

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,8 +2,7 @@ import { PageInfo } from '../typings'
 
 export async function fetchPageInfo() {
     const res =  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
-    const data = await res.json()
-
+    const data = await (await res).json()
     const pageInfo: PageInfo = data.pageInfo
 
     return pageInfo
