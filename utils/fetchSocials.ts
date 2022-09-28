fd1528613d184c4abb11f6f784457fc78a4c4778
pryority <matthewapryor@gmail.com>
2022-09-28 15:52:15 -0400

    remove base url

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,7 +1,7 @@
 import { Social } from '../typings'
 
 export async function fetchSocials() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`)
+    const res = await fetch(`/api/getSocials`)
     const data = await res.json()
     const socials: Social[] = data.socials
 
