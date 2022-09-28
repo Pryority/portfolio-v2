c0fa868acd11434b47a35d4cefdffa50f0673b82
pryority <matthewapryor@gmail.com>
2022-09-28 14:56:39 -0400

    fix: rever to commit 34f0caa5b6b4cf2942f6f3998c7aaea22cfc6c53

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -2,7 +2,6 @@ import { Social } from '../typings'
 
 export async function fetchSocials() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`)
-
     const data = await res.json()
     const socials: Social[] = data.socials
 
