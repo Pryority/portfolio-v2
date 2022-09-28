ac2430218684ef9e5a1c8feed72f06e6de24ca23
pryority <matthewapryor@gmail.com>
2022-09-28 14:59:40 -0400

    fix: site loads again, no errors -- still cannot deploy

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,7 +2,6 @@ import { Project } from '../typings'
 
 export async function fetchProjects() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
-
     const data = await res.json()
     const projects: Project[] = data.projects
 
