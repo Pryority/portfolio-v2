4aab8a8b9bfee5379b3be5df43c925b91ffd5ece
pryority <matthewapryor@gmail.com>
2022-10-04 15:18:12 -0400

    fn change

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,6 +1,6 @@
 import { Project } from '../typings'
 
-export async function fetchProjects() {
+export const fetchProjects = async() => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
     const data = await res.json()
     const projects: Project[] = data.projects
