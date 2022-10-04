d7f06ffb9c9830a1a627bb19070b731a33482b41
pryority <matthewapryor@gmail.com>
2022-10-04 17:46:18 -0400

    rename

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,6 +92,6 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       projects,
       socials
     },
-    revalidate: 60
+    revalidate: 10
   }
 }
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,6 +2,7 @@ import { PageInfo } from '../typings'
 
 export const fetchPageInfo = async () => {
     const res =  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
+    console.log(res)
     const data = await res.json()
     const pageInfo: PageInfo = data.pageInfo
 
