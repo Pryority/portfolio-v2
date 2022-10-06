f754152b5396a0aeb87c5831dcbd1e90f6c4a07a
pryority <matthewapryor@gmail.com>
2022-10-06 12:44:03 -0400

    fixing api url to fixed vercal app url

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -5,6 +5,6 @@ export const fetchSkills = async () => {
     const res = await fetch(`${server}/api/getSkills`)
     const data = await res.json()
     const skills: Skill[] = data.skills
-
+    console.log(process.env.VERCEL_GIT_REPO_SLUG)
     return skills
 }
