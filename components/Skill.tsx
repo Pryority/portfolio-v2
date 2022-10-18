1fac07937502ebe71d57897c2974c94a00da805a
pryority <matthewapryor@gmail.com>
2022-10-18 02:42:55 -0400

    fix: minor styling

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -27,10 +27,10 @@ export default function Skill({skill, directionLeft}: Props) {
             />
         </motion.div>
         <motion.div 
-            initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1 }}
-            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: .162 }}
+            viewport={{ once: false }}
             className='absolute object-center opacity-1 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white/80 hidden group-hover:block h-24 w-24 md:w-28 md:h-28 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
